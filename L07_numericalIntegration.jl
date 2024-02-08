0c9ce760dca2850b185301a6f6d309d9816853c4
TommyNHL <research.tommyn.lok@gmail.com>
2024-02-08 21:43:29 +0100

    L08

---
+++ L07_numericalIntegration.jl
@@ -0,0 +1,10 @@
+# Numerical Integration
+## - aim to find AUC without calculating the analytical derivative of f(x)
+## - assume f(x) is continuous between a and b
+## Integration( f(x) * dx )
+
+### Midpoint method
+# - 
+
+### Trapezoid rule
+### Simpson's rule
